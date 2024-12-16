243bb5387477bd2d7999c118cab169949f7e8e6c
jzone677 <jasperpwiggles.thecoder@gmail.com>
2024-12-16 12:31:42 -0500

    Update game.h to fix the beep function

--- game.h
+++ game.h
@@ -2,17 +2,14 @@
 #include <stdio.h>
 #include <ctype.h>
 #include <conio.h>
-#def mainf void main(){
-#def forever while(1){\nclrscr();
-#def end }
-#include <stdio.h>
-#include <stdlib.h>
-#include <unistd.h>
+
+#define mainf void main(){
+#define forever while(1){ clrscr();
+#define end }
 
 void beep(int duration, int frequency) {
     int i;
     for (i = 0; i < duration * frequency; i++) {
         printf("\a");
-        usleep(1000000 / frequency);
     }
 }
