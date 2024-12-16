00614dee0b8c5f70796c20bea0a229261b7f6ca9
jzone677 <jasperpwiggles.thecoder@gmail.com>
2024-12-16 09:55:46 -0500

    Update game.h to add a beep function

--- game.h
+++ game.h
@@ -5,3 +5,14 @@
 #def mainf void main(){
 #def forever while(1){\nclrscr();
 #def end }
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+void beep(int duration, int frequency) {
+    int i;
+    for (i = 0; i < duration * frequency; i++) {
+        printf("\a");
+        usleep(1000000 / frequency);
+    }
+}
